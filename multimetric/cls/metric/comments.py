2aff12c4ec571b356465dcb1c8fe9794a0916da9
Konrad Weihmann <kweihmann@outlook.com>
2019-12-30 16:29:52 +0100

    Fix comment ratio calc
    - mind any language specific stuff

--- multimetric/cls/metric/comments.py
+++ multimetric/cls/metric/comments.py
@@ -6,11 +6,16 @@ class MetricBaseComments(MetricBase):
         "Token.Comment",
         "Token.Comment.Hashbang",
         "Token.Comment.Multiline",
-        "Token.Comment.Preproc",
         "Token.Comment.Single",
         "Token.Comment.Special",
         "Token.Literal.String.Doc"
     ]
+
+    _specific = {
+        "Python": [
+            "Token.Comment.Preproc"
+        ]
+    }
 
     METRIC_COMMENT_RATIO = "comment_ratio"
 
@@ -21,10 +26,13 @@ class MetricBaseComments(MetricBase):
 
     def parse_tokens(self, language, tokens):
         super().parse_tokens(language, [])
+        _n = MetricBaseComments._needles
+        if language in MetricBaseComments._specific:
+            _n += MetricBaseComments._specific[language]
         for x in tokens:
             self.__overall += len(str(x[1]))
-            if str(x[0]) in MetricBaseComments._needles:
-                self.__comments += len(str(x[0]))
+            if str(x[0]) in _n:
+                self.__comments += len(str(x[1]))
 
     def get_results(self):
         if self.__overall == 0:
