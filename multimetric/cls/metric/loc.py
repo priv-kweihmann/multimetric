b64a08ce2b4c2368f13acbbd845393d6baa10d73
Konrad Weihmann <kweihmann@outlook.com>
2020-02-08 16:37:14 +0100

    Fix LOC metric
    Just count the newlines
    
    Closes #9

--- multimetric/cls/metric/loc.py
+++ multimetric/cls/metric/loc.py
@@ -2,24 +2,19 @@ from multimetric.cls.base import MetricBase
 
 
 class MetricBaseLOC(MetricBase):
+    METRIC_LOC = "loc"
+
     _needles = [
         "Token.Text"
     ]
-    _contents = [
-        '\n',
-        '\r\n'
-    ]
-
-    METRIC_LOC = "loc"
 
     def __init__(self, args, **kwargs):
         super().__init__(args, **kwargs)
 
     def parse_tokens(self, language, tokens):
         super().parse_tokens(language, [])
-        if not any(tokens):
-            # to avoid a log(0) the minimum of loc is 1
-            self._metrics[MetricBaseLOC.METRIC_LOC] = 1
-        else:
-            self._metrics[MetricBaseLOC.METRIC_LOC] = len([x for x in tokens if str(
-                x[0]) in MetricBaseLOC._needles and x[1] in MetricBaseLOC._contents]) + 1
+        self._metrics[MetricBaseLOC.METRIC_LOC] = 0
+        for x in tokens:
+            if str(x[0]) in MetricBaseLOC._needles:
+                self._metrics[MetricBaseLOC.METRIC_LOC] += len([y for y in x[1] if y == '\n'])
+        self._metrics[MetricBaseLOC.METRIC_LOC] = max(self._metrics[MetricBaseLOC.METRIC_LOC], 1)
