be526c7bf671e47328365a685a4edbda95af3615
Konrad Weihmann <kweihmann@outlook.com>
2021-04-16 12:31:02 +0200

    Remove tests
    as they aren't working anyway
    
    Signed-off-by: Konrad Weihmann <kweihmann@outlook.com>

--- release.sh
+++ release.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 python3 setup.py build
 python3 setup.py sdist
-./test.sh || exit 1
 twine upload dist/*
--- test.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-export PATH=$(pwd)/bin:${PATH}
-for _test in $(pwd)/tests/*.test; do
-    /bin/bash ${_test} || true
-done
