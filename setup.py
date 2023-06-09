1ae3cafb4d7d7f261babccb4de3ec55c84046d90
Jason Nichols <github@ayl.us>
2023-06-09 15:29:58 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ with open('requirements.txt') as f:
 
 setuptools.setup(
     name="multimetric",
-    version="1.3.1",
+    version="1.4.1",
     author="Jason Nichols",
     author_email="jn@ayl.us",
     description="Calculate code metrics in various languages",
