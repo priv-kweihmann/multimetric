d5bf52d518c7706936a7feb1f8a0204c9ffa884d
Jason Nichols <jason@startupos.dev>
2023-06-24 10:00:20 -0400

    readme and setup

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ with open('requirements.txt') as f:
 
 setuptools.setup(
     name="modernmetric",
-    version="1.4.1",
+    version="1.0.0",
     author="Jason Nichols",
     author_email="jason@startupos.dev",
     description="Calculate code metrics in various languages",
