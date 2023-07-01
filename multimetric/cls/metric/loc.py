ee8dc414ae056b32044e67af818ae7ddff8bcc7e
Konrad Weihmann <kweihmann@outlook.com>
2023-07-01 07:00:08 +0000

    loc: fix lines of code detection
    a line of code is when a token ends with '\n' and
    the previous token is not '\n'.
    Add tests for all languages under test to ensure that
    
    Fixes #30
    
    Signed-off-by: Konrad Weihmann <kweihmann@outlook.com>

--- multimetric/cls/metric/loc.py
+++ multimetric/cls/metric/loc.py
@@ -6,17 +6,23 @@ class MetricBaseLOC(MetricBase):
 
     _needles = [
         "Token.Text",
+        "Token.Comment.Preproc",
+        "Token.Text.Whitespace",
     ]
 
     def __init__(self, args, **kwargs):
         super().__init__(args, **kwargs)
+        self._previous_token = (None, None)
 
     def parse_tokens(self, language, tokens):
         super().parse_tokens(language, [])
         self._metrics[MetricBaseLOC.METRIC_LOC] = 0
         for x in tokens:
-            if str(x[0]) in MetricBaseLOC._needles:
-                self._metrics[MetricBaseLOC.METRIC_LOC] += len([y for y in x[1] if y == '\n'])
+            if self._previous_token != x:
+                if x[1].strip(' ').endswith('\n'):
+                    self._metrics[MetricBaseLOC.METRIC_LOC] += 1
+            self._previous_token = x
+            
         self._metrics[MetricBaseLOC.METRIC_LOC] = max(self._metrics[MetricBaseLOC.METRIC_LOC], 1)
         self._internalstore["loc"] = self._metrics[MetricBaseLOC.METRIC_LOC]
 
--- tests/test_integration.py
+++ tests/test_integration.py
@@ -18,173 +18,225 @@ class TestClassIntegration():
             file = [file]
 
         args_ = parse_args(file)
-        return (run(args_), file)
+        loc = []
+        for f in file:
+            with open(f) as i:
+                loc.append(sum(1 for x in i.read() if x == '\n'))
+        return (run(args_), file, loc)
     
     def test_bash(self, file_BASH):
-        result, files = self._run(file_BASH)
+        result, files, loc = self._run(file_BASH)
 
-        for f in files:
+        for index, f in enumerate(files):
             assert 'Bash' in result.get('files', {}).get(f, {}).get('lang', [])
             assert result.get('files', {}).get(f, {}).get('operands_sum', 0) > 0
+            if loc[index] > 1:
+                assert result.get('files', {}).get(f, {}).get('loc', 0) > 1
 
     def test_c(self, file_C):
-        result, files = self._run(file_C)
+        result, files, loc = self._run(file_C)
 
-        for f in files:
+        for index, f in enumerate(files):
             assert 'C' in result.get('files', {}).get(f, {}).get('lang', [])
             assert result.get('files', {}).get(f, {}).get('operands_sum', 0) > 0
+            if loc[index] > 1:
+                assert result.get('files', {}).get(f, {}).get('loc', 0) > 1
 
     def test_cpp(self, file_CPP):
-        result, files = self._run(file_CPP)
+        result, files, loc = self._run(file_CPP)
 
-        for f in files:
+        for index, f in enumerate(files):
             assert 'C++' in result.get('files', {}).get(f, {}).get('lang', [])
             assert result.get('files', {}).get(f, {}).get('operands_sum', 0) > 0
+            if loc[index] > 1:
+                assert result.get('files', {}).get(f, {}).get('loc', 0) > 1
 
     def test_coffescript(self, file_COFFEESCRIPT):
-        result, files = self._run(file_COFFEESCRIPT)
+        result, files, loc = self._run(file_COFFEESCRIPT)
 
-        for f in files:
+        for index, f in enumerate(files):
             assert 'CoffeeScript' in result.get('files', {}).get(f, {}).get('lang', [])
             assert result.get('files', {}).get(f, {}).get('operands_sum', 0) > 0
+            if loc[index] > 1:
+                assert result.get('files', {}).get(f, {}).get('loc', 0) > 1
     
     def test_csharp(self, file_CSHARP):
-        result, files = self._run(file_CSHARP)
+        result, files, loc = self._run(file_CSHARP)
 
-        for f in files:
+        for index, f in enumerate(files):
             assert 'C#' in result.get('files', {}).get(f, {}).get('lang', [])
             assert result.get('files', {}).get(f, {}).get('operands_sum', 0) > 0
+            if loc[index] > 1:
+                assert result.get('files', {}).get(f, {}).get('loc', 0) > 1
 
     def test_dart(self, file_DART):
-        result, files = self._run(file_DART)
+        result, files, loc = self._run(file_DART)
 
-        for f in files:
+        for index, f in enumerate(files):
             assert 'Dart' in result.get('files', {}).get(f, {}).get('lang', [])
             assert result.get('files', {}).get(f, {}).get('operands_sum', 0) > 0
+            if loc[index] > 1:
+                assert result.get('files', {}).get(f, {}).get('loc', 0) > 1
 
     def test_go(self, file_GO):
-        result, files = self._run(file_GO)
+        result, files, loc = self._run(file_GO)
 
-        for f in files:
+        for index, f in enumerate(files):
             assert 'Go' in result.get('files', {}).get(f, {}).get('lang', [])
+            if loc[index] > 1:
+                assert result.get('files', {}).get(f, {}).get('loc', 0) > 1
             # FIXME!
             #assert result.get('files', {}).get(f, {}).get('operands_sum', 0) > 0
 
     def test_groovy(self, file_GROOVY):
-        result, files = self._run(file_GROOVY)
+        result, files, loc = self._run(file_GROOVY)
 
-        for f in files:
+        for index, f in enumerate(files):
             assert 'Groovy' in result.get('files', {}).get(f, {}).get('lang', [])
             assert result.get('files', {}).get(f, {}).get('operands_sum', 0) > 0
+            if loc[index] > 1:
+                assert result.get('files', {}).get(f, {}).get('loc', 0) > 1
 
     def test_haskell(self, file_HASKELL):
-        result, files = self._run(file_HASKELL)
+        result, files, loc = self._run(file_HASKELL)
 
-        for f in files:
+        for index, f in enumerate(files):
             assert 'Haskell' in result.get('files', {}).get(f, {}).get('lang', [])
             assert result.get('files', {}).get(f, {}).get('operands_sum', 0) > 0
+            if loc[index] > 1:
+                assert result.get('files', {}).get(f, {}).get('loc', 0) > 1
 
     def test_java(self, file_JAVA):
-        result, files = self._run(file_JAVA)
+        result, files, loc = self._run(file_JAVA)
 
-        for f in files:
+        for index, f in enumerate(files):
             assert 'Java' in result.get('files', {}).get(f, {}).get('lang', [])
             assert result.get('files', {}).get(f, {}).get('operands_sum', 0) > 0
+            if loc[index] > 1:
+                assert result.get('files', {}).get(f, {}).get('loc', 0) > 1
     
     def test_javascript(self, file_JAVASCRIPT):
-        result, files = self._run(file_JAVASCRIPT)
+        result, files, loc = self._run(file_JAVASCRIPT)
 
-        for f in files:
+        for index, f in enumerate(files):
             assert 'JavaScript' in result.get('files', {}).get(f, {}).get('lang', [])
             assert result.get('files', {}).get(f, {}).get('operands_sum', 0) > 0
+            if loc[index] > 1:
+                assert result.get('files', {}).get(f, {}).get('loc', 0) > 1
 
     def test_julia(self, file_JULIA):
-        result, files = self._run(file_JULIA)
+        result, files, loc = self._run(file_JULIA)
 
-        for f in files:
+        for index, f in enumerate(files):
             assert 'Julia' in result.get('files', {}).get(f, {}).get('lang', [])
             assert result.get('files', {}).get(f, {}).get('operands_sum', 0) > 0
+            if loc[index] > 1:
+                assert result.get('files', {}).get(f, {}).get('loc', 0) > 1
 
     def test_kotlin(self, file_KOTLIN):
-        result, files = self._run(file_KOTLIN)
+        result, files, loc = self._run(file_KOTLIN)
 
-        for f in files:
+        for index, f in enumerate(files):
             assert 'Kotlin' in result.get('files', {}).get(f, {}).get('lang', [])
             assert result.get('files', {}).get(f, {}).get('operands_sum', 0) > 0
+            if loc[index] > 1:
+                assert result.get('files', {}).get(f, {}).get('loc', 0) > 1
 
     def test_lisp(self, file_LISP):
-        result, files = self._run(file_LISP)
+        result, files, loc = self._run(file_LISP)
 
-        for f in files:
+        for index, f in enumerate(files):
             assert 'NewLisp' in result.get('files', {}).get(f, {}).get('lang', [])
             assert result.get('files', {}).get(f, {}).get('operands_sum', 0) > 0
+            if loc[index] > 1:
+                assert result.get('files', {}).get(f, {}).get('loc', 0) > 1
 
     def test_lua(self, file_LUA):
-        result, files = self._run(file_LUA)
+        result, files, loc = self._run(file_LUA)
 
-        for f in files:
+        for index, f in enumerate(files):
             assert 'Lua' in result.get('files', {}).get(f, {}).get('lang', [])
             assert result.get('files', {}).get(f, {}).get('operands_sum', 0) > 0
+            if loc[index] > 1:
+                assert result.get('files', {}).get(f, {}).get('loc', 0) > 1
 
     def test_objectivec(self, file_OBJECTIVEC):
-        result, files = self._run(file_OBJECTIVEC)
+        result, files, loc = self._run(file_OBJECTIVEC)
 
-        for f in files:
+        for index, f in enumerate(files):
             assert 'Objective-C' in result.get('files', {}).get(f, {}).get('lang', [])
             assert result.get('files', {}).get(f, {}).get('operands_sum', 0) > 0
+            if loc[index] > 1:
+                assert result.get('files', {}).get(f, {}).get('loc', 0) > 1
 
     def test_perl(self, file_PERL):
-        result, files = self._run(file_PERL)
+        result, files, loc = self._run(file_PERL)
 
-        for f in files:
+        for index, f in enumerate(files):
             assert 'Prolog' in result.get('files', {}).get(f, {}).get('lang', [])
             assert result.get('files', {}).get(f, {}).get('operands_sum', 0) > 0
+            if loc[index] > 1:
+                assert result.get('files', {}).get(f, {}).get('loc', 0) > 1
 
     def test_php(self, file_PHP):
-        result, files = self._run(file_PHP)
+        result, files, loc = self._run(file_PHP)
 
-        for f in files:
+        for index, f in enumerate(files):
             assert 'PHP' in result.get('files', {}).get(f, {}).get('lang', [])
             assert result.get('files', {}).get(f, {}).get('operands_sum', 0) > 0
+            if loc[index] > 1:
+                assert result.get('files', {}).get(f, {}).get('loc', 0) > 1
 
     def test_python(self, file_PYTHON):
-        result, files = self._run(file_PYTHON)
+        result, files, loc = self._run(file_PYTHON)
 
-        for f in files:
+        for index, f in enumerate(files):
             assert 'Python' in result.get('files', {}).get(f, {}).get('lang', [])
             assert result.get('files', {}).get(f, {}).get('operands_sum', 0) > 0
+            if loc[index] > 1:
+                assert result.get('files', {}).get(f, {}).get('loc', 0) > 1
 
     def test_ruby(self, file_RUBY):
-        result, files = self._run(file_RUBY)
+        result, files, loc = self._run(file_RUBY)
 
-        for f in files:
+        for index, f in enumerate(files):
             assert 'Ruby' in result.get('files', {}).get(f, {}).get('lang', [])
             assert result.get('files', {}).get(f, {}).get('operands_sum', 0) > 0
+            if loc[index] > 1:
+                assert result.get('files', {}).get(f, {}).get('loc', 0) > 1
 
     def test_rust(self, file_RUST):
-        result, files = self._run(file_RUST)
+        result, files, loc = self._run(file_RUST)
 
-        for f in files:
+        for index, f in enumerate(files):
             assert 'Rust' in result.get('files', {}).get(f, {}).get('lang', [])
             assert result.get('files', {}).get(f, {}).get('operands_sum', 0) > 0
+            if loc[index] > 1:
+                assert result.get('files', {}).get(f, {}).get('loc', 0) > 1
 
     def test_tcl(self, file_TCL):
-        result, files = self._run(file_TCL)
+        result, files, loc = self._run(file_TCL)
 
-        for f in files:
+        for index, f in enumerate(files):
             assert 'Tcl' in result.get('files', {}).get(f, {}).get('lang', [])
             assert result.get('files', {}).get(f, {}).get('operands_sum', 0) > 0
+            if loc[index] > 1:
+                assert result.get('files', {}).get(f, {}).get('loc', 0) > 1
 
     def test_typescript(self, file_TYPESCRIPT):
-        result, files = self._run(file_TYPESCRIPT)
+        result, files, loc = self._run(file_TYPESCRIPT)
 
-        for f in files:
+        for index, f in enumerate(files):
             assert 'TypeScript' in result.get('files', {}).get(f, {}).get('lang', [])
             assert result.get('files', {}).get(f, {}).get('operands_sum', 0) > 0
+            if loc[index] > 1:
+                assert result.get('files', {}).get(f, {}).get('loc', 0) > 1
 
     def test_zig(self, file_ZIG):
-        result, files = self._run(file_ZIG)
+        result, files, loc = self._run(file_ZIG)
 
-        for f in files:
+        for index, f in enumerate(files):
             assert 'Zig' in result.get('files', {}).get(f, {}).get('lang', [])
             assert result.get('files', {}).get(f, {}).get('operands_sum', 0) > 0
+            if loc[index] > 1:
+                assert result.get('files', {}).get(f, {}).get('loc', 0) > 1
