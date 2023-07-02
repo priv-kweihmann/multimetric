d50142a7349f0481b3abb4d01b90e5f5a1b8c53f
Konrad Weihmann <kweihmann@outlook.com>
2023-07-02 09:35:47 +0000

    tokentree: add new class
    which allows us to have a reusable code base
    to scan through a token tree with a fixed
    configuration and extract the needed information.
    
    Adjust fanout class in that way to make use of this
    new feature
    
    Signed-off-by: Konrad Weihmann <kweihmann@outlook.com>

--- multimetric/cls/metric/fanout.py
+++ multimetric/cls/metric/fanout.py
@@ -1,23 +1,38 @@
 from multimetric.cls.base import MetricBase
+from multimetric.cls.tokentree import TokenTree
+from multimetric.cls.tokentree import TokenTreeConfig
 
 
 class MetricBaseFanout(MetricBase):
-    _needles = {
-        "Python": [
-            "Token.Name.Namespace",
-        ],
-        "C": [
-            "Token.Comment.PreprocFile",
-        ],
-        "C++": [
-            "Token.Comment.PreprocFile",
-        ],
+    _config = {
+        'Go': TokenTreeConfig(
+            start=[('Token.Keyword.Namespace', 'import')],
+            end=[('Token.Punctuation', ')')],
+            needle=['Token.Literal.String'],
+            trim=['"', "'"]),
+        'PHP': TokenTreeConfig(
+            start=[('Token.Keyword', 'include'), ('Token.Keyword', 'require'),
+                   ('Token.Keyword', 'include_once'), ('Token.Keyword', 'require_once')],
+            end=[('Token.Literal.String.Single', ''), ('Token.Literal.String.Double', '')],
+            needle=['Token.Literal.String'],
+            trim=['"', "'"]),
+        'Ruby': TokenTreeConfig(
+            start=[('Token.Name.Builtin', 'require')],
+            end=[('Token.Text.Whitespace', ')')],
+            needle=['Token.Literal', 'Token.Literal.String.Double'],
+            trim=['"', "'"]),
+        'Python': TokenTreeConfig(
+            start=[('Token.Keyword.Namespace', 'import'), ('Token.Keyword.Namespace', 'from')],
+            end=[('Token.Text', '\n')],
+            needle=['Token.Name.Namespace', 'Token.Keyword.Namespace', 'Token.Name'],
+            trim=[]),
+        'default': TokenTreeConfig(
+            start=[('Token.Comment.Preproc', 'include'), ('Token.Comment.Namespace', '')],
+            end=[('Token.Text.Whitespace', '\n'), ('Token.Comment.Preproc', '\n')],
+            needle=['Token.Comment.PreprocFile'],
+            trim=[]),
     }
-    _functions = {
-        "PHP": "_parsePHP",
-        "Go": "_parseGo",
-        "Ruby": "_parseRuby",
-    }
+
     _internal = {
         "Python": {"start": ".", "end": ""},
         "C": {"start": "\"", "end": "\""},
@@ -36,63 +51,16 @@ class MetricBaseFanout(MetricBase):
         return all([value.startswith(internal_mapping["start"]),
                     value.endswith(internal_mapping["end"])])
 
-    def _parsePHP(self, iterator):
-        res = []
-        _start_token = ["include", "require", "include_once", "require_once"]
-        _cont_token = ["Token.Literal.String.Single", "Token.Literal.String.Double"]
-        for _, value in iterator:
-            if str(value[0]) in ["Token.Keyword"] and value[1] in _start_token:
-                while iterator and str(value[0]) not in _cont_token:
-                    try:
-                        _, value = next(iterator)
-                    except StopIteration:
-                        break
-                if iterator:
-                    res.append(value[1].strip("'").strip('"'))
-        return res
-
-    def _parseRuby(self, iterator):
-        res = []
-        for i, value in iterator:
-            if str(value[0]) in ["Token.Name.Builtin"] and value[1] in ["require"]:
-                while iterator:
-                    i, value = next(iterator)
-                    if str(value[0]) in ["Token.Literal.String.Single"]:
-                        res.append(value[1].strip("'").strip('"'))
-                        break
-                    elif str(value[0]) in ["Token.Text"] and value[1] in ["\n", "\r\n"]:
-                        break
-        return res
-
-    def _parseGo(self, iterator):
-        res = []
-        for _, value in iterator:
-            if str(value[0]) in ["Token.Keyword.Namespace"] and value[1] in ["import"]:
-                while iterator:
-                    try:
-                        _, value = next(iterator)
-                        if str(value[0]) in ["Token.Literal.String"]:
-                            res.append(value[1].strip("'").strip('"'))
-                        if str(value[0]) in ["Token.Punctuation"] and value[1] in [')']:
-                            break
-                    except StopIteration:
-                        break
-        return res
-
     def parse_tokens(self, language, tokens):
         super().parse_tokens(language, [])
         if language in MetricBaseFanout._internal:
             _i = MetricBaseFanout._internal[language]
         else:
             _i = {"start": "", "end": ""}
-        _imports = []
-        if language in MetricBaseFanout._needles.keys():
-            _n = MetricBaseFanout._needles[language]
-            for x in [x for x in tokens if str(x[0]) in _n]:
-                _imports.append(x[1])
-        elif language in MetricBaseFanout._functions:
-            _imports = getattr(self,
-                               MetricBaseFanout._functions[language])(enumerate(tokens))
+
+        config = MetricBaseFanout._config.get(language, MetricBaseFanout._config['default'])
+        _imports = TokenTree.get_from_token_tree(enumerate(tokens), config)
+
         for x in _imports:
             if self.__isInternal(x, _i):
                 self._int.add(str(x))
---
+++ multimetric/cls/tokentree.py
@@ -0,0 +1,65 @@
+# SPDX-FileCopyrightText: 2023 Konrad Weihmann
+# SPDX-License-Identifier: Zlib
+
+import logging
+from enum import Enum
+from typing import List
+from typing import Tuple
+
+
+class TokenTreeConfig():
+
+    def __init__(self,
+                 start: List[Tuple[str, str]],
+                 end: List[Tuple[str, str]],
+                 needle: List[str],
+                 trim: List[str]) -> None:
+        self.start = start
+        self.end = end
+        self.needle = needle
+        self.trim = trim
+
+    def match(self, token, config_item: List[Tuple[str, str]]) -> bool:
+        for item in config_item:
+            type_, val = item
+            if str(token[0]).startswith(str(type_)) and (val == token[1] or not val):
+                return True
+        return False
+
+
+class TokenTree():
+    class TreeState(Enum):
+        START = 0
+        INBLOCK = 1
+
+    @staticmethod
+    def get_from_token_tree(iterator, config: TokenTreeConfig) -> List[str]:
+        state = TokenTree.TreeState.START
+        result = set()
+        last_hit = []
+        for _, value in iterator:
+            if state == TokenTree.TreeState.START:
+                if config.match(value, config.start):
+                    logging.debug(f'Match start: {value}')
+                    state = TokenTree.TreeState.INBLOCK
+            elif state == TokenTree.TreeState.INBLOCK:
+                if config.match(value, config.end):
+                    logging.debug(f'Match end: {value}')
+                    state = TokenTree.TreeState.START
+                    if last_hit:
+                        result.add(' '.join(last_hit))
+                    last_hit = []
+                if any(str(value[0]).startswith(x) for x in config.needle):
+                    logging.debug(f'Match needle: {value}')
+                    _extracted_value = value[1]
+                    for i in config.trim:
+                        _extracted_value = _extracted_value.strip(i)
+                    if _extracted_value:
+                        last_hit.append(_extracted_value)
+                elif str(value[0]) in ['Token.Text.Whitespace'] and value[1].strip(' ').endswith('\n'):
+                    logging.debug(f'Inblock line end: {value} -> {last_hit}')
+                    if last_hit:
+                        result.add(' '.join(last_hit))
+                    last_hit = []
+        logging.debug(f'Found {result}')
+        return result
