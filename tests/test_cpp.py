20874f785ecc95b55e772a2035cc9b90832ee07f
Konrad Weihmann <kweihmann@outlook.com>
2023-07-02 12:08:52 +0000

    tests: add cpp test
    Signed-off-by: Konrad Weihmann <kweihmann@outlook.com>

---
+++ tests/test_cpp.py
@@ -0,0 +1,56 @@
+# SPDX-FileCopyrightText: 2023 Konrad Weihmann
+# SPDX-License-Identifier: Zlib
+import os
+
+import pytest  # noqa: I900
+
+
+class TestClassCPP():
+
+    def _run(self, in_):
+        from multimetric.__main__ import parse_args
+        from multimetric.__main__ import run
+
+        file = in_
+
+        if isinstance(file, str):
+            file = [file]
+
+        args_ = parse_args(file)
+        loc = []
+        for f in file:
+            with open(f) as i:
+                loc.append(sum(1 for x in i.read() if x == '\n'))
+        return (run(args_), file, loc)
+    
+    def test_cpp(self):
+        file = os.path.join(pytest.test_dir_local, 'test.cpp')
+
+        res, _, _ = self._run(file)
+
+        assert 'C++' in res.get('files', {}).get(file, {}).get('lang', [])
+        assert res.get('files', {}).get(file, {}).get('comment_ratio', 0) == 4.805
+        assert res.get('files', {}).get(file, {}).get('cyclomatic_complexity', 0) == 8
+        assert res.get('files', {}).get(file, {}).get('fanout_external', 0) == 1
+        assert res.get('files', {}).get(file, {}).get('fanout_internal', 0) == 0
+        assert res.get('files', {}).get(file, {}).get('halstead_bugprop', 0) == 0.329
+        assert res.get('files', {}).get(file, {}).get('halstead_difficulty', 0) == 44.571
+        assert res.get('files', {}).get(file, {}).get('halstead_effort', 0) == 43960.132
+        assert res.get('files', {}).get(file, {}).get('halstead_timerequired', 0) == 2442.23
+        assert res.get('files', {}).get(file, {}).get('halstead_volume', 0) == 986.285
+        assert res.get('files', {}).get(file, {}).get('loc', 0) == 31
+        assert res.get('files', {}).get(file, {}).get('maintainability_index', 0) == 100
+        assert res.get('files', {}).get(file, {}).get('operands_sum', 0) == 78
+        assert res.get('files', {}).get(file, {}).get('operands_uniq', 0) == 14
+        assert res.get('files', {}).get(file, {}).get('operators_sum', 0) == 123
+        assert res.get('files', {}).get(file, {}).get('operators_uniq', 0) == 16
+        assert res.get('files', {}).get(file, {}).get('pylint', 0) == 100.0
+        assert res.get('files', {}).get(file, {}).get('tiobe', 0) == 86.901
+        assert res.get('files', {}).get(file, {}).get('tiobe_compiler', 0) == 100.0
+        assert res.get('files', {}).get(file, {}).get('tiobe_complexity', 0) == 12.673
+        assert res.get('files', {}).get(file, {}).get('tiobe_coverage', 0) == 100.0
+        assert res.get('files', {}).get(file, {}).get('tiobe_duplication', 0) == 100.0
+        assert res.get('files', {}).get(file, {}).get('tiobe_fanout', 0) == 100.0
+        assert res.get('files', {}).get(file, {}).get('tiobe_functional', 0) == 100.0
+        assert res.get('files', {}).get(file, {}).get('tiobe_security', 0) == 100.0
+        assert res.get('files', {}).get(file, {}).get('tiobe_standard', 0) == 100.0
