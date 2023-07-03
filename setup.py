382d8cb0cbeb28bd5341824207b3b8d3ad4f6619
priv-kweihmann <priv-kweihmann@localhost>
2023-07-03 14:01:30 +0000

    Release 2.0.0

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ with open('requirements.txt') as f:
 
 setuptools.setup(
     name="multimetric",
-    version="1.3.0",
+    version="2.0.0",
     author="Konrad Weihmann",
     author_email="kweihmann@outlook.com",
     description="Calculate code metrics in various languages",
