e7db2cfe34c81858512844622be777f6088a46cf
Konrad Weihmann <kweihmann@outlook.com>
2023-07-03 05:05:19 +0000

    tokentree: add support for regex
    to allow even more combinations instead of relying
    only on plain string matching.
    This will help if the tokenizer does only return complex
    structures.
    
    Also add include_start to config, to include the
    starting token to the result.
    Generalize the trim functionality and use replace
    not strip for strings, as otherwise every character from
    the string would be removed and this is not what
    we want
    
    Signed-off-by: Konrad Weihmann <kweihmann@outlook.com>

--- multimetric/cls/tokentree.py
+++ multimetric/cls/tokentree.py
@@ -2,6 +2,7 @@
 # SPDX-License-Identifier: Zlib
 
 import logging
+import re
 from enum import Enum
 from typing import List
 from typing import Tuple
@@ -13,16 +14,18 @@ class TokenTreeConfig():
                  start: List[Tuple[str, str]],
                  end: List[Tuple[str, str]],
                  needle: List[str],
-                 trim: List[str]) -> None:
+                 trim: List[str],
+                 include_start: bool = False) -> None:
         self.start = start
         self.end = end
         self.needle = needle
         self.trim = trim
+        self.include_start = include_start
 
     def match(self, token, config_item: List[Tuple[str, str]]) -> bool:
         for item in config_item:
             type_, val = item
-            if str(token[0]).startswith(str(type_)) and (val == token[1] or not val):
+            if str(token[0]).startswith(str(type_)) and (re.match(val, token[1]) or not val):
                 return True
         return False
 
@@ -37,10 +40,24 @@ class TokenTree():
         state = TokenTree.TreeState.START
         result = set()
         last_hit = []
+
+        def cleanup(item: str, config: TokenTreeConfig) -> str:
+            for i in config.trim:
+                if len(i) == 1:
+                    item = item.strip(i)
+                else:
+                    item = item.replace(i, '')
+            return item
+
         for _, value in iterator:
             if state == TokenTree.TreeState.START:
                 if config.match(value, config.start):
                     logging.getLogger('stderr').debug(f'Match start: {value}')
+                    if config.include_start:
+                        item = cleanup(value[1], config)
+                        if item:
+                            logging.getLogger('stderr').debug(f'Adding chunk: {item}')
+                            last_hit.append(item)
                     state = TokenTree.TreeState.INBLOCK
             elif state == TokenTree.TreeState.INBLOCK:
                 if config.match(value, config.end):
@@ -51,11 +68,10 @@ class TokenTree():
                     last_hit = []
                 if any(str(value[0]).startswith(x) for x in config.needle):
                     logging.getLogger('stderr').debug(f'Match needle: {value}')
-                    _extracted_value = value[1]
-                    for i in config.trim:
-                        _extracted_value = _extracted_value.strip(i)
-                    if _extracted_value:
-                        last_hit.append(_extracted_value)
+                    item = cleanup(value[1], config)
+                    if item:
+                        logging.getLogger('stderr').debug(f'Adding chunk: {item}')
+                        last_hit.append(item)
                 elif str(value[0]) in ['Token.Text.Whitespace'] and value[1].strip(' ').endswith('\n'):
                     logging.getLogger('stderr').debug(f'Inblock line end: {value} -> {last_hit}')
                     if last_hit:
