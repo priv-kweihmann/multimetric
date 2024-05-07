d3c74925dcf5a49c028be3fdf7cc36684487320e
Konrad Weihmann <kweihmann@outlook.com>
2024-05-07 07:23:22 +0200

    tests: updates
    Signed-off-by: Konrad Weihmann <kweihmann@outlook.com>

--- tests/test_rust.py
+++ tests/test_rust.py
@@ -33,17 +33,17 @@ class TestClassRust():
         assert res.get('files', {}).get(file, {}).get('cyclomatic_complexity', 0) == 4
         assert res.get('files', {}).get(file, {}).get('fanout_external', 0) == 3
         assert res.get('files', {}).get(file, {}).get('fanout_internal', 0) == 0
-        assert res.get('files', {}).get(file, {}).get('halstead_bugprop', 0) == 0.504
-        assert res.get('files', {}).get(file, {}).get('halstead_difficulty', 0) == 29.934
-        assert res.get('files', {}).get(file, {}).get('halstead_effort', 0) == 45268.064
-        assert res.get('files', {}).get(file, {}).get('halstead_timerequired', 0) == 2514.892
-        assert res.get('files', {}).get(file, {}).get('halstead_volume', 0) == 1512.252
+        assert res.get('files', {}).get(file, {}).get('halstead_bugprop', 0) == 0.556
+        assert res.get('files', {}).get(file, {}).get('halstead_difficulty', 0) == 33.526
+        assert res.get('files', {}).get(file, {}).get('halstead_effort', 0) == 55930.369
+        assert res.get('files', {}).get(file, {}).get('halstead_timerequired', 0) == 3107.243
+        assert res.get('files', {}).get(file, {}).get('halstead_volume', 0) == 1668.253
         assert res.get('files', {}).get(file, {}).get('loc', 0) == 42
-        assert res.get('files', {}).get(file, {}).get('maintainability_index', 0) == 71.459
+        assert res.get('files', {}).get(file, {}).get('maintainability_index', 0) == 70.948
         assert res.get('files', {}).get(file, {}).get('operands_sum', 0) == 91
         assert res.get('files', {}).get(file, {}).get('operands_uniq', 0) == 38
-        assert res.get('files', {}).get(file, {}).get('operators_sum', 0) == 162
-        assert res.get('files', {}).get(file, {}).get('operators_uniq', 0) == 25
+        assert res.get('files', {}).get(file, {}).get('operators_sum', 0) == 185
+        assert res.get('files', {}).get(file, {}).get('operators_uniq', 0) == 28
         assert res.get('files', {}).get(file, {}).get('pylint', 0) == 100.0
         assert res.get('files', {}).get(file, {}).get('tiobe', 0) == 93.807
         assert res.get('files', {}).get(file, {}).get('tiobe_compiler', 0) == 100.0
